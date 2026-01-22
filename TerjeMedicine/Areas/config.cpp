9fca116e3c066d27ec67c07414d1ccda34c02b99
Streatman <TheStreatman@gmail.com>
2026-01-22 08:56:21 +0100

    Update config.cpp

--- TerjeMedicine/Areas/config.cpp
+++ TerjeMedicine/Areas/config.cpp
@@ -7,7 +7,8 @@ class CfgPatches
 		requiredVersion=0.1;
 		requiredAddons[]=
 		{
-			"TerjeMedicine", "TerjeCore"
+			"TerjeMedicine",
+			"TerjeCore"
 		};
 	};
 };
@@ -18,10 +19,10 @@ class CfgVehicles
 	class TerjePsionicScriptableArea: TerjeScriptableArea
 	{
 		scope=2;
-		terjeInnerRadius = 50.0; // Players inside this radius will receive full damage
-		terjeOuterRadius = 150.0; // Players inside this radius will receive proportional damage depends on distance between two radiuses
-		terjeHeightMin = -100.0; // Lower range of the area cylinder
-		terjeHeightMax = 100.0; // Upper range of the area cylinder
-		terjePower = 2.0; // Area power modifier
+		terjeInnerRadius=50.0; // Players inside this radius will receive full damage
+		terjeOuterRadius=150.0; // Players inside this radius will receive proportional damage depends on distance between two radiuses
+		terjeHeightMin=-100.0; // Lower range of the area cylinder
+		terjeHeightMax=100.0; // Upper range of the area cylinder
+		terjePower=2.0; // Area power modifier
 	};
 };
