49007ba332f02856f1c4ffbc05d827f1f05144b8
sysrover <sysrover@gmail.com>
2026-01-25 11:20:54 -0500

    Add ability to hide skills from UI
    Introduces a SKILLS_HIDDEN_SKILLS map and related settings to allow specific skills to be hidden from the UI. Updates skill initialization and display logic in RightArea and TerjeSkillsContainer to respect the hidden status, hiding widgets and the container if all skills are hidden.

--- TerjeSkills/Scripts/5_Mission/RightArea.c
+++ TerjeSkills/Scripts/5_Mission/RightArea.c
@@ -22,11 +22,30 @@ modded class RightArea
 		}
 		else if (m_terjeSkillsContainer == null && GetTerjeSkillsRegistry().IsReadyToDisplay())
 		{
+			int settingKey;
+			bool settingValue;
+			array<ref TerjeSkillCfg> registeredSkills();
+			GetTerjeSkillsRegistry().GetSkills(registeredSkills);
+			int registeredSkillsCount = registeredSkills.Count();
+
+			// Count hidden skills
+			foreach (ref TerjeSkillCfg skill : registeredSkills)
+			{
+				if (TerjeSettingsCollection.SKILLS_HIDDEN_SKILLS.Find(skill.GetId(), settingKey) && GetTerjeSettingBool(settingKey, settingValue) && settingValue)
+				{
+					registeredSkillsCount--;
+				}
+			}
+
 			PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
 			if (player)
 			{
 				m_terjeSkillsContainer = new TerjeSkillsContainer(this);
 				m_terjeSkillsContainer.InitializeSkills(player);
+				
+				// If no skills to show, return
+				if (registeredSkillsCount <= 0)
+					m_terjeSkillsContainer.GetMainWidget().Show(false);
 				m_Body.Insert( m_terjeSkillsContainer );
 				RecomputeOpenedContainers();
 			}
