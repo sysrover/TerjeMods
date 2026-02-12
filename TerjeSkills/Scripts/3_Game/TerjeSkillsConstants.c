3daf12f7a9bc877c620c948eed81e09177620a1d
Streatman <TheStreatman@gmail.com>
2026-02-12 21:45:03 +0100

    Merge pull request #7 from sysrover/COT_Perk_admintool
    Refactor and extend Terje COT integration and skills

--- TerjeSkills/Scripts/3_Game/TerjeSkillsConstants.c
+++ TerjeSkills/Scripts/3_Game/TerjeSkillsConstants.c
@@ -2,4 +2,5 @@ class TerjeSkillsConstants
 {
 	const string TRPC_PLAYER_PERK_APPLY = "~ts_perk_apply";
 	const string TRPC_PLAYER_PERKS_RESET = "~ts_perk_reset";
+	const string TRPC_PLAYER_PERK_DEC = "~ts_perk_dec";
 }
