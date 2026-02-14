026c90466ed12018678fc762e4b04382e3b070e0
Streatman <TheStreatman@gmail.com>
2026-02-15 00:00:15 +0100

    Update CoughSymptom.c

--- TerjeMedicine/Scripts/4_World/Classes/PlayerSymptoms/CoughSymptom.c
+++ TerjeMedicine/Scripts/4_World/Classes/PlayerSymptoms/CoughSymptom.c
@@ -2,7 +2,7 @@ modded class CoughSymptom
 {
 	override bool IsContaminationActive()
 	{
-		if (m_Player && m_Player.GetTerjeStats().GetBiohazardValue() >= 1)
+		if (m_Player && m_Player.GetTerjeStats() && m_Player.GetTerjeStats().GetBiohazardValue() >= 1)
 			return true;
 		return false;
 	}
